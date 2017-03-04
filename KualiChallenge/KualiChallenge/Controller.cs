da85bc275453dc5acaa5e0c8d1ff76c748e43301
Todd Webb <webbjtodd@gmail.com>
2017-03-03 19:16:34 -0700

    15 minute checkins Initial Elevator and Controller classes

---
+++ KualiChallenge/KualiChallenge/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KualiChallenge
+{
+    public class Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="elevators"></param>
+        public Controller(int elevators)
+        {
+
+        }
+
+        #region properties
+        #endregion
+    }
+}
---
+++ KualiChallenge/KualiChallenge/Elevator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KualiChallenge
+{
+    public class Elevator
+    {
+
+        /// <summary>
+        /// Constructor
+        /// Initialize with the given number of floors
+        /// Initialize at floor 1 with the door closed
+        /// Initialize with 0 trips
+        /// Initialize with 0 floors passed
+        /// </summary>
+        /// <param name="floors"></param>
+        public Elevator(int floors)
+        {
+            
+            Floors = floors;
+            CurrentFloor = 1;
+            IsDoorOpen = false;
+            Trips = 0;
+            FloorsPassed = 0;
+        }
+
+        #region Properties
+        int Floors { get; set; }
+        int CurrentFloor { get; set; }
+        bool IsDoorOpen { get; set; }
+        int Trips { get; set; }
+        int FloorsPassed { get; set; }
+
+        #endregion
+
+    }
+}
